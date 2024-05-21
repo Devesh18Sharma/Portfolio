50ee16ca6de8e1b28fd571863db90210a01366c6
Devesh Sharma <97972618+Devesh18Sharma@users.noreply.github.com>
2024-05-21 21:57:07 +0530

    Portfolio Website

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building SAAS Automation web-app",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Kadam Shoes",
+    des: "Ecommerce Shoes-App/Platform for Sales Boost",
+    img: "/kadam-shoes.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    href: "https://kadam-shoes.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Meme-Creator App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/meme-creator.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    href: "https://meme-creator-app-m6pb.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Fusion Vault",
+    des: "Created FusionVault offering a global space for tech enthusiasts worldwide to collaborate.",
+    img: "/Fusion-Vault.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    href: "https://github.com/Devesh18Sharma/FusionVault",
+  },
+  {
+    id: 4,
+    title: "Automate_Hub",
+    des: "Created a SAAS Automation platform where business owners can automate their business.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    href: "https://github.com/Devesh18Sharma",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Devesh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Devesh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Devesh is the ideal partner.",
+    name: "Pritish Maheta",
+    title: "Co-Founder @Xcelcode",
+  },
+  {
+    quote:
+      "I've collaborated with numerous developers, but Devesh's exceptional skills and professional demeanor set him apart. His promptness and commitment to excellence were evident from day one. Devesh's enthusiasm for full-stack development, combined with his innovative problem-solving abilities, resulted in a website that not only met our needs but also elevated our brand's digital presence. ",
+    name: "Malav Shah",
+    title: "Founder @Immersfy",
+  },
+  {
+    quote:
+      "Partnering with Devesh was an incredible experience. His professional approach, punctuality, and unwavering dedication to delivering top-notch results were evident throughout our collaboration. Devesh's enthusiasm for every detail of development shines through. If you're looking to upgrade your website and strengthen your brand, Devesh is the ideal choice.",
+    name: "Siddarth Sharma",
+    title: "SDE-2 @Rooter.gg",
+  },
+  {
+    quote:
+      "Mentoring Devesh has been an incredibly rewarding experience. As his college senior, I've seen firsthand his impressive dedication and rapid growth in full-stack development. Devesh consistently delivers high-quality results with professionalism and creativity. His contributions have significantly enhanced our website and brand presence. Devesh is a standout developer who brings both passion and expertise to any project.",
+    name: "Guneet Singh",
+    title: "SDE-1 @Cobalt",
+  },
+  {
+    quote:
+      "It has been a privilege to mentor Devesh and witness his growth as a developer. Working as an SDE-1 at Microsoft, I have seen many talented individuals, but Devesh's enthusiasm and dedication to full-stack development are truly exceptional. His professionalism, promptness, and creative problem-solving have consistently impressed me. Devesh's contributions have not only elevated our project's website but also reinforced our brand's reputation. I highly recommend Devesh for any project; his passion and skills make him a standout developer.",
+    name: "Amogh Sachdeva",
+    title: "SDE-1 @Microsoft",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full-Stack SDE Intern",
+    desc: "Assisted in the development of a web-based platform and enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Technial Head- Quintessence",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a Ecommerce Web-app for a client, from initial concept to deployment.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/Devesh18Sharma",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    href: "https://twitter.com/devesh181002",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/devesh-sharma-87242a14b/",
+  },
+];
